bfb9de0e201eee6d2f9153fc5ebae6ae795672b9
Jason Graziano <mrehawk82@gmail.com>
2017-08-19 16:38:28 -0400

    trying to figure out how to refresh the page

--- SolarBLE/SolarBLE.Shared/pages/hub/section4Page.js
+++ SolarBLE/SolarBLE.Shared/pages/hub/section4Page.js
@@ -17,23 +17,32 @@
     // The following lines expose this control constructor as a global. 
     // This lets you use the control as a declarative control inside the 
     // data-win-control attribute.
-    var mainSect4;
+    var dtaIn, mainSect4;
+
+    dtaIn = 12;
 
     mainSect4 = {
-        0: "Main Section 4",
-        1: "1st Section 4",
-        2: "2nd Section 4",
-        3: "3rd Section 4"
+        0: "<button class='backBtn'>Close</button><h2>You and your system</h2><br /><p>You have ran your system for " + dtaIn + " days straight!</p>",
+        1: "<button class='backBtn'>Close</button><h2>Information about your personal carbon footprint</h2><br /><p></p>",
+        2: "<button class='backBtn'>Close</button><h2>Information about your personal acheivements</h2><br /><p></p>",
+        3: "<button class='backBtn'>Close</button><h2>Advice from Skye</h2><br /><p></p>"
     };
 
     function triggerMain(k, element, imgClassSect4) {
         return function () {
             //console.log(imgClassSect4[k]);
 
-            imgClassSect4[3].style.display = "none";
-            imgClassSect4[2].style.display = "none";
-            imgClassSect4[1].style.display = "none";
-            imgClassSect4[0].style.display = "none";
+            imgClassSect4[3].style.marginLeft = "100vw";
+
+
+            imgClassSect4[2].style.marginLeft = "100vw";
+
+
+            imgClassSect4[1].style.marginLeft = "100vw";
+
+
+            imgClassSect4[0].style.marginLeft = "100vw";
+
 
             var slate = document.createElement("div");
                 
@@ -41,14 +50,36 @@
 
                 slate.innerHTML = mainSect4[k];
 
-                document.body.appendChild(slate);
+                document.body.appendChild(slate); 
 
-                setTimeout(function () { slate.className = "slate_full"; }, .1);
-                
+                setTimeout(function () {
+                    var backBtn = document.querySelector(".backBtn");
+                    backBtn.addEventListener("click", closeMain(slate), false);
+
+                    
+
+                    slate.className = "slate_full";
+                }, .1);
+                function closeMain(slate) {
+                    return function () {
+                        imgClassSect4[3].style.marginLeft = "0";
+
+
+                        imgClassSect4[2].style.marginLeft = "0";
+
+
+                        imgClassSect4[1].style.marginLeft = "0";
+
+
+                        imgClassSect4[0].style.marginLeft = "0";
+                        slate.removeNode();
+                        //window.location.reload();
+                    }
+                }
          
-        } 
+        }
     }
-
+    
     WinJS.Namespace.define("HubApps_SectionControls", {
         Section4Control: ControlConstructor
     });
